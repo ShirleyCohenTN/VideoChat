7364f8391a5e390caf8257d5b650a3d91a2c0d74
Shirley Cohen <ShilyChanXD@hotmail.com>
2022-07-27 14:02:49 +0300

    moved css file to the right folder

--- video_chat/frontend/src/index.jsx
+++ video_chat/frontend/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 import { ContextProvider } from "./SocketContext";
-import "./styles.css";
+import "./css/styles.css";
 
 ReactDOM.render(
   <ContextProvider>
